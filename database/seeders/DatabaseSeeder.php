e9fccd4e4ebf1dd4072fd42d589af84213cfa202
Mostafa Elbagory <zxsofazx@gmail.com>
2024-11-18 01:06:39 +0200

    x

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call([
-             AdminRoleTable::class,
+         //    AdminRoleTable::class,
              AdminTable::class,
              SellerTableSeeder::class
          ]);
